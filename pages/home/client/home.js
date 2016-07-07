828089aeaa88eaebdfae8fc2e0cf8a7ea4c70978
VanioSantos <vanio@brandeis.edu>
2016-07-06 21:05:05 -0400

    finished login/register system [basic implementation - needs email verification, validations, etc]

--- pages/home/client/home.js
+++ pages/home/client/home.js
@@ -4,32 +4,31 @@ Template.home.helpers({
 
 
 Template.home.events({
-            "submit .js-register": function(event) {
-                event.preventDefault();
+    "submit .js-register": function(event) {
+        event.preventDefault();
 
-                const f = $(".js-first-name").val();
-                const l = $(".js-last-name").val();
-                const e = $(".js-email").val();
-                const p = $(".js-password").val();
+        const f = $(".js-first-name").val();
+        const l = $(".js-last-name").val();
+        const e = $(".js-email").val();
+        const p = $(".js-password").val();
 
-                Accounts.createUser({
-                            profile: {
-                                first: f,
-                                last: l
-                            },
-                            email: e,
-                            password: p
-                        })
-                    
-                    function(error) {
-                        if (error) { // if registration fails
-                            console.log(error.reason); // console log reason [show display error to user]
-                            return;
-                        } else {
-                            Router.go('/'); // else go to ??? page
-                        }
-
-                        alert("You're registered!");
-                    }
+        Accounts.createUser({
+                profile: {
+                    first: f,
+                    last: l
+                },
+                email: e,
+                password: p
+            }),
+            function(error) {
+                if (error) { // if registration fails
+                    console.log(error.reason); // console log reason [show display error to user]
+                    return;
+                } else {
+                    Router.go('/'); // else go to ??? page
                 }
-                    });
+
+                alert("You're registered!");
+            }
+    }
+});
---
+++ pages/login/client/login.js
@@ -0,0 +1,23 @@
+Template.login.helpers({
+
+});
+
+Template.login.events({
+    "submit .js-login": function() {
+        event.preventDefault();
+
+        const e = $(".js-email-login").val();
+        const p = $(".js-password-login").val();
+
+        Meteor.loginWithPassword(e, p, function(error) {
+            if (error) {
+                event.preventDefault();
+                console.log(error.reason); // // console log reason [show display error to user]
+                return;
+            } else {
+                alert("You are now logged in.");
+                // Router.go("");
+            }
+        });
+    },
+});

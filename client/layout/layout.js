74d995d19dfb6898b1df626e0e22d3b33172285a
VanioSantos <vanio@brandeis.edu>
2016-07-10 11:23:11 -0400

    added side nav w/ 32x32 test icons + tweaked upcoming page + added routing restriction when logged in

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -15,24 +15,4 @@ Template.layout.events({
         Router.go('/');
     },
 
-    "click .js-more-btn": function() {
-        event.preventDefault();
-
-        $(".css-side-nav").animate({
-            left: "+=300"
-        }, 1000, function() {
-            // Animation complete.
-        });
-
-    },
-
-    // $("#clickme").click(function() {
-    //     $("#book").animate({
-    //         opacity: 0.25,
-    //         left: "+=50",
-    //         height: "toggle"
-    //     }, 5000, function() {
-    //         // Animation complete.
-    //     });
-    // });
 });

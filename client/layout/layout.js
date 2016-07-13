69a26ff80e326a033e64ec46af73370cab76de9a
sylvialin22 <sblin@brandeis.edu>
2016-07-13 14:48:25 -0400

    added modal for creation of task, goal, text layout

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -4,7 +4,7 @@ Template.layout.helpers({
         return Meteor.users.findOne({
             _id: liveUser
         });
-    }
+    },
 });
 
 Template.layout.events({
@@ -14,5 +14,52 @@ Template.layout.events({
         Meteor.logout();
         Router.go('/');
     },
+    // "click .js-modal": function() {
+    //     event.preventDefault();
+    //
+    // },
 
+});
+
+Template.modal.helpers({
+  taskChosen: function(){
+    return Template.instance().taskChosen.get();
+  },
+  goalChosen: function(){
+    return Template.instance().goalChosen.get();
+  },
+  textChosen: function(){
+    return Template.instance().textChosen.get();
+  },
+});
+
+
+Template.modal.events({
+  'change select': function(event, template){
+    if($(event.target).val()=="task"){
+      template.taskChosen.set(true);
+      template.goalChosen.set(false);
+      template.textChosen.set(false);
+
+    }else if($(event.target).val()=="goal"){
+      template.taskChosen.set(false);
+      template.goalChosen.set(true);
+      template.textChosen.set(false);
+    }else if($(event.target).val()=="text"){
+      template.taskChosen.set(false);
+      template.goalChosen.set(false);
+      template.textChosen.set(true);
+    }
+  }
+});
+
+Template.modal.onCreated(function(){
+  this.taskChosen = new ReactiveVar(true);
+  this.goalChosen = new ReactiveVar(false);
+  this.textChosen = new ReactiveVar(false);
+
+});
+
+Template.modal.onRendered(function() {
+    this.$('.datetimepicker5').datetimepicker();
 });

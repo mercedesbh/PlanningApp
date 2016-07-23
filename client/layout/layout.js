790b9fd96bed54f3ae55cda2945e13cb329bf219
sylvialin22 <sblin@brandeis.edu>
2016-07-23 13:48:16 -0400

    completed geocoding of destination in map

--- client/layout/layout.js
+++ client/layout/layout.js
@@ -277,7 +277,7 @@ function calculateRoute(from, to) {
             if (status == google.maps.DirectionsStatus.OK) {
                 console.log("routing");
                 new google.maps.DirectionsRenderer({
-                    map: GoogleMaps.maps.initMap.instance,
+                    map: GoogleMaps.maps.initMap,
                     directions: response
                 });
             } else
@@ -286,43 +286,11 @@ function calculateRoute(from, to) {
     );
 }
 
-$(document).ready(function() {
-    // If the browser supports the Geolocation API
-    if (typeof navigator.geolocation == "undefined") {
-        $("#error").text("Your browser doesn't support the Geolocation API");
-        return;
-    }
-
-    $("#from-link, #to-link").click(function(event) {
-        event.preventDefault();
-        var addressId = this.id.substring(0, this.id.indexOf("-"));
-
-        navigator.geolocation.getCurrentPosition(function(position) {
-                var geocoder = new google.maps.Geocoder();
-                geocoder.geocode({
-                        "location": new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
-                    },
-                    function(results, status) {
-                        if (status == google.maps.GeocoderStatus.OK)
-                            $("#" + addressId).val(results[0].formatted_address);
-                        else
-                            $("#error").append("Unable to retrieve your address<br />");
-                    });
-            },
-            function(positionError) {
-                $("#error").append("Error: " + positionError.message + "<br />");
-            }, {
-                enableHighAccuracy: true,
-                timeout: 10 * 1000 // 10 seconds
-            });
-    });
-
     $("#calculate-route").submit(function(event) {
         event.preventDefault();
         calculateRoute($("#from").val(), $("#to").val());
     });
 
-});
 
 Template.modal.onCreated(function() {
     this.taskChosen = new ReactiveVar(true);
@@ -418,6 +386,8 @@ function calculateRoute(origin, destination) {
                 map: GoogleMaps.maps.initMap.instance,
                 directions: response
               });
+              getLatlong();
+
               console.log("complete");
             }
             else
@@ -425,6 +395,22 @@ function calculateRoute(origin, destination) {
           }
         );
       }
+      function getLatlong(){
+          var geocoder = new google.maps.Geocoder();
+          var address = document.getElementById('end').value;
+
+          geocoder.geocode({ 'address': address }, function (results, status) {
+
+              if (status == google.maps.GeocoderStatus.OK) {
+                  var latitude = results[0].geometry.location.lat();
+                  var longitude = results[0].geometry.location.lng();
+                  console.log([latitude, longitude]);
+
+              }
+          });
+
+      }
+
 
       $(document).ready(function() {
         // If the browser supports the Geolocation API
